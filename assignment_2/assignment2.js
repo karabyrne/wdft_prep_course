12b0dc1b1ee2ab2577a49723e8e473c862742501
karabyrne <karabyrne@users.noreply.github.com>
2017-05-13 15:01:31 -0400

    Assignment 2 done!

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,6 +3,10 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
+for(var i=1;i<=100;i++){
+    console.log(i);
+}
+
 
 
 /*
@@ -17,3 +21,8 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+var hashes = "";
+for(var i=1;i<=7;i++){
+    hashes += "#";
+    console.log(hashes);
+}
